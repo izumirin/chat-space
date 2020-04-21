d77bc8318f73b4571410f13201962cbc89465806
izumirin <mariya_0903@icloud.com>
2020-04-21 22:18:38 +0900

    改善

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,66 +1,51 @@
-$(function(){ 
+$(function(){
+
   function buildHTML(message){
-   if ( message.image ) {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-         <img src=${message.image} >
-       </div>`
-     return html;
-   } else {
-     var html =
-      `<div class="message" data-message-id=${message.id}>
-         <div class="upper-message">
-           <div class="upper-message__user-name">
-             ${message.user_name}
-           </div>
-           <div class="upper-message__date">
-             ${message.date}
-           </div>
-         </div>
-         <div class="lower-message">
-           <p class="lower-message__content">
-             ${message.content}
-           </p>
-         </div>
-       </div>`
-     return html;
-   };
- }
-$('.js-form').on('submit', function(e){
- e.preventDefault();
- var formData = new FormData(this);
- var url = $(this).attr('action')
- $.ajax({
-   url: url,
-   type: "POST",
-   data: formData,
-   dataType: 'json',
-   processData: false,
-   contentType: false
- })
-  .done(function(data){
-    var html = buildHTML(data);
-    $('.messages').append(html);
-    $('form')[0].reset();
-    $('input').prop('disabled', false);
-    $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 500);
-  })
-   .fail(function(){
-     alert('error');
-   });
-   return false;
- });
+    image = ( message.image ) ? `<img class="lower-message__image" src=${message.image} >` : ""; //三項演算子
+                var html =  
+                ` <div class="message" data-message-id="${message.id}">
+                <div class="upper-message">
+                  <div class="upper-message__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="upper-message__date">
+                    ${message.date}
+                  </div>
+                </div>
+                <div class="lower-message">
+                  <p class="lower-message__content">
+                  ${message.content}
+                  ${image}
+                  </p>
+                </div>
+              </div>`
+          return html; 
+  }
+
+
+
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html);
+      $("form")[0].reset();
+      $('input').prop('disabled', false);
+      $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 50);
+    })
+      .fail(function() {
+        alert("メッセージ送信に失敗しました");
+    });
+  });
+
 });
