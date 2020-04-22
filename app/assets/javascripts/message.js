2922ffed7a25a6a940dbd01696bb6ce77df988e0
izumirin <mariya_0903@icloud.com>
2020-04-22 19:16:18 +0900

    kaizenn

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,5 +1,6 @@
 $(function(){
 
+
   function buildHTML(message){
     image = ( message.image ) ? `<img class="lower-message__image" src=${message.image} >` : ""; //三項演算子
                 var html =  
@@ -47,5 +48,34 @@ $(function(){
         alert("メッセージ送信に失敗しました");
     });
   });
+  var reloadMessages = function() {
+    var last_message_id = $('.message:last').data("message-id");
+    $.ajax({
+      url: "api/messages",
+      type: 'get',
+      dataType: 'json',
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      if (messages.length !== 0) {
+      var insertHTML = '';
+      $.each(messages, function(i, message) {
+        insertHTML += buildHTML(message)
+      });
+      $('.messages').append(insertHTML);
+      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+    $(function() {
+        setInterval(reloadMessages, 7000);
+      });
+      if (document.location.href.match(/\/groups\/\d+\/messages/)) {
+        setInterval(reloadMessages, 7000);
+      }
+      
+  };
 
 });
