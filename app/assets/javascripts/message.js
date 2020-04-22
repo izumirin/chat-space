ca116db4201f762022e99df9530fc29600bc3270
izumirin <mariya_0903@icloud.com>
2020-04-22 19:53:20 +0900

    kaizzen

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -47,35 +47,31 @@ $(function(){
       .fail(function() {
         alert("メッセージ送信に失敗しました");
     });
-  });
   var reloadMessages = function() {
-    var last_message_id = $('.message:last').data("message-id");
-    $.ajax({
-      url: "api/messages",
-      type: 'get',
-      dataType: 'json',
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      if (messages.length !== 0) {
-      var insertHTML = '';
-      $.each(messages, function(i, message) {
-        insertHTML += buildHTML(message)
-      });
-      $('.messages').append(insertHTML);
-      $('.messages').animate({ scrollTop: $('.messages')[0].scrollHeight});
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-    $(function() {
-        setInterval(reloadMessages, 7000);
-      });
-      if (document.location.href.match(/\/groups\/\d+\/messages/)) {
-        setInterval(reloadMessages, 7000);
-      }
+    if (window.location.href.match(/\/groups\/\d+\/messages/)){
+      var last_message_id = $('.message:last').data("message-id"); 
       
+
+      $.ajax({ 
+        url: "api/messages", 
+        type: 'get', 
+        dataType: 'json', 
+        data: {last_id: last_message_id} 
+      })
+      .done(function (messages) { 
+        var insertHTML = '';
+        messages.forEach(function (message) {
+          insertHTML = buildHTML(message); 
+          $('.messages').append(insertHTML);
+        })
+        $('.messages').animate({scrollTop: $('.messages')[0].scrollHeight}, 'fast');
+      })
+      .fail(function () {
+        alert('自動更新に失敗しました');
+      });
+    }
   };
+  setInterval(reloadMessages, 5000);
+  });
 
 });
